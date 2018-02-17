63014cd8fa8e39c48514b80c4e5622b75f8809c0
Ivo van der Wijk <github@in.m3r.nl>
2018-02-17 18:26:24 +0100

    control volume

--- drivers/frontier-silicon-speaker/device.js
+++ drivers/frontier-silicon-speaker/device.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Homey = require('homey');
+const request = require('request-promise-native');
+const xml2js = require('xml2js');
 
 class MyDevice extends Homey.Device {
 
@@ -15,6 +17,26 @@ class MyDevice extends Homey.Device {
         this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this))
         this.registerCapabilityListener('volume_set', this.onCapabilityVolumeSet.bind(this))
         this.registerCapabilityListener('volume_mute', this.onCapabilityVolumeMute.bind(this))
+
+        let data = this.getData();
+        let ip = data.ip;
+        this.pin = "1234";
+
+        request({
+            url: `http://${ip}/fsapi/CREATE_SESSION`,
+            qs: { pin: this.pin}
+        }).then(res => {
+            this.log("Create session result", res);
+            xml2js.parseString(res, (error, result) => {
+                if(error) {
+                    this.log("Error parsing session id", error);
+                    // reject
+                }
+                this.log("parsed", result);
+                this.sessionid = result.fsapiResponse.sessionId[0];
+                this.log("Session id", this.sessionid);
+            });
+        });
     }
 
     // this method is called when the Device is added
@@ -27,8 +49,24 @@ class MyDevice extends Homey.Device {
         this.log('device deleted');
     }
 
+    invokeApi(operation, value) {
+        let data = this.getData();
+        let ip = data.ip;
+
+        let url = `http://${ip}/fsapi/${operation}`;
+
+        request({url: url, qs: {
+            pin: this.pin,
+            sid: this.sessionid,
+            value: value
+        }});
+
+    }
     onCapabilityVolumeSet( value, opts, callback ) {
 
+        let level = value * 20;
+
+        this.invokeApi("SET/netRemote.sys.audio.volume", level.toString())
         // ... set value to real device
         this.log("OnCapabilityVolumeSet", value, opts);
 
